3987f0d3826a2f0d1e9e638c56fbe059937e8537
ayebaishmo <ishmo255super@gmail.com>
2024-04-09 12:12:18 +0300

    Fixed lint errors

--- sprint1/bloomdata.py
+++ sprint1/bloomdata.py
@@ -1,18 +1,43 @@
+"""
+This module demonstrates:
+increament a a number by 1
+decrease a a number by 1
+count the elements in an array
+Sums up all the elements in an array
+
+They are within a function
+"""
+
 
 
 def increment(num):
+    """
+    The above function 
+    increament a a number by 1 
+    """
     return num  + 1
 
-
 def decrease(num):
+    """
+    The above function 
+    decreases a a number by 1 
+    """
     return num - 1
 
 
-def cout(a):
-    return len(a)
+def cout(array_1):
+    """
+    The above function 
+    counts the of elements in array
+    """
+    return len(array_1)
 
-def ar(b):
-    sum = 0
-    for x in b:
-        sum +=x
+def add_up(array_2):
+    """
+    The above function 
+    counts the of elements in array
+    """
+    sum_up = 0
+    for ele_arr in array_2:
+        sum_up +=ele_arr
     return sum
--- sprint1/test_bloomdata.py
+++ sprint1/test_bloomdata.py
@@ -1,26 +1,54 @@
+"""
+This module tests
+bloodata function of they are running well
+"""
 import pytest
+
 import sprint1.bloomdata as sblm
 
 #the function name has to start with test
 def test_increment_int():
+    """
+    The function checks if the increment 
+    function parameter is given a 3 gives a 4
+    """
     #assert statement is important
     assert sblm.increment(3) == 4
     assert sblm.increment(-2) == -1
 
 def test_increament_float():
+    """
+    The function checks if the increment 
+    function parameter is given a 1.5 gives a 2.5
+    """
     assert sblm.increment(1.5) == 2.5
-
 
 # check the data type passed
 def test_increment_int_return_type():
+    """
+    The function checks if the 
+    the return statement is an integer
+    """
     assert isinstance(sblm.increment(3), int)
 
 def test_decrease_int():
+    """
+    The function checks if the decrease 
+    function parameter is given a 3 gives a 2
+    """
     assert sblm.decrease(3) == 2
 
 def test_count_a():
+    """
+    The function checks if the
+    length is 4
+    """
     assert sblm.cout([1,2,3,4]) == 4
 
 
 def test_sum():
-    assert sblm.ar([4,5,6]) == 15
+    """
+    The function checks if the sum 
+    function is 15
+    """
+    assert sblm.add_up([4,5,6]) == 15
